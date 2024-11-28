0d3af63dccc37342cf180a031ea6c66ba7af3a37
maylodossantos <maylo.santos2004@gmail.com>
2024-11-28 17:32:01 -0300

    Commit - 5

--- src/Customer.java
+++ src/Customer.java
@@ -26,19 +26,18 @@ public class Customer {
         while (rentals.hasMoreElements()) {
             Rental each = (Rental) rentals.nextElement();
 
-            // calculate amount for each rental
-            double thisAmount = each.getCharge();
-
             // add frequent renter points
             frequentRenterPoints++;
             // add bonus for a two-day new release rental
             if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE) &&
-                    each.getDaysRented() > 1) frequentRenterPoints++;
+                    each.getDaysRented() > 1) {
+                frequentRenterPoints++;
+            }
 
             // show figures for this rental
             result += "\t" + each.getMovie().getTitle() + "\t" +
-                    String.valueOf(thisAmount) + "\n";
-            totalAmount += thisAmount;
+                    String.valueOf(each.getCharge()) + "\n";
+            totalAmount += each.getCharge();
         }
 
         // add footer lines
