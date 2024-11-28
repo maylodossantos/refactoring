25859d82944dd7d3e86fe69087fed99d8c9f0d5c
maylodossantos <maylo.santos2004@gmail.com>
2024-11-28 17:38:39 -0300

    Commit - 9

--- src/Rental.java
+++ src/Rental.java
@@ -1,40 +1,20 @@
 public class Rental {
     private Movie _movie;
     private int _daysRented;
-
     public Rental(Movie movie, int daysRented) {
         _movie = movie;
         _daysRented = daysRented;
     }
-
     public int getDaysRented() {
         return _daysRented;
     }
-
     public Movie getMovie() {
         return _movie;
     }
-
     public double getCharge() {
-        double thisAmount = 0;
-        switch (_movie.getPriceCode()) {
-            case Movie.REGULAR:
-                thisAmount += 2;
-                if (_daysRented > 2)
-                    thisAmount += (_daysRented - 2) * 1.5;
-                break;
-            case Movie.NEW_RELEASE:
-                thisAmount += _daysRented * 3;
-                break;
-            case Movie.CHILDRENS:
-                thisAmount += 1.5;
-                if (_daysRented > 3)
-                    thisAmount += (_daysRented - 3) * 1.5;
-                break;
-        }
-        return thisAmount;
+        // Now delegate the charge calculation to Movie class
+        return _movie.getCharge(_daysRented);
     }
-
     // Extracted and moved method
     public int getFrequentRenterPoints() {
         // Check if the movie is a new release and rented for more than 1 day
