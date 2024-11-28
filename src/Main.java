bf161d9a38379a70550823a6876e20b6355e4d80
maylodossantos <maylo.santos2004@gmail.com>
2024-11-28 16:32:48 -0300

    base-version

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+
+    public static void main(String[] args) {
+        Customer newCostumer = new Customer("Maylo");
+
+        System.out.printf(newCostumer.getName());
+    }
+}
